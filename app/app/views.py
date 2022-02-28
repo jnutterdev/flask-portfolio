7db7c25ff09f04e413cd2eecf8f532c21b9188d7
jnutterdev <hello@jnutterdev.com>
2022-02-28 16:33:17 -0500

    started working on forms and posting data

--- app/app/views.py
+++ app/app/views.py
@@ -1,5 +1,8 @@
 from app import app
 from flask import render_template
+from datetime import datetime
+from flask import request, redirect
+
 
 @app.route("/")
 def index():
@@ -8,3 +11,76 @@ def index():
 @app.route("/about")
 def about():
     return render_template("public/about.html")
+
+@app.template_filter("clean_date")
+def clean_date(dt):
+    return dt.strftime("%d %b %Y")
+
+@app.route("/sign-up", methods=["GET", "POST"])
+def sign_up():
+
+    if request.method == "POST":
+
+        req = request.form
+        print(req)
+
+        return redirect(request.url)
+
+    return render_template("public/sign_up.html")
+
+@app.route("/jinja")
+def jinja():
+    # Strings
+    my_name = "John"
+
+    # Integers
+    my_age = 41
+
+    # Lists
+    langs = ["Python", "JavaScript", "Bash", "Ruby"]
+
+    # Dictionaries
+    friends = {
+        "Erika": 42,
+        "Cody": 28,
+        "Amy": 26,
+        "Clarissa": 23,
+        "Wendell": 39
+    }
+
+    # Tuples
+    colors = ("Red", "Blue")
+
+    # Booleans
+    cool = True
+
+    # Classes
+    class GitRemote:
+        def __init__(self, name, description, domain):
+            self.name = name
+            self.description = description 
+            self.domain = domain
+
+        def pull(self):
+            return f"Pulling repo '{self.name}'"
+
+        def clone(self, repo):
+            return f"Cloning into {repo}"
+
+    my_remote = GitRemote(
+        name="Learning Flask",
+        description="Learn the Flask web framework for Python",
+        domain="https://github.com/Julian-Nash/learning-flask.git"
+    )
+
+    # Functions
+    def repeat(x, qty=1):
+        return x * qty
+
+    date = datetime.utcnow()
+
+    return render_template(
+        "public/jinja.html", my_name=my_name, my_age=my_age, langs=langs,
+        friends=friends, colors=colors, cool=cool, GitRemote=GitRemote, 
+        my_remote=my_remote, repeat=repeat, date=date
+    )
